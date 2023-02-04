60d7a5657ff9bde5e498219fa8f5e4b51ac9076a
jstevens515 <115838886+jeremystevens515@users.noreply.github.com>
2023-02-04 17:50:51 -0500

    added model classes and defined associations

---
+++ server.js
@@ -0,0 +1,19 @@
+require('dotenv').config();
+const express = require('express');
+const routes = require('./Develop/routes/index');
+// import sequelize connection
+const sequelize = require('./Develop/config/connection');
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+// sync sequelize models to the database, then turn on the server
+sequelize.sync({force: true}).then(() => {
+  app.listen(PORT, () => console.log(`App listening on port ${PORT}!`));
+});
+
